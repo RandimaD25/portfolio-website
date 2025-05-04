fcd8b8790302edb2d068dd2852c70c61ea98d058
RandimaD25 <randimamethminid525@gmail.com>
2025-05-04 16:44:27 +0530

    feat: change some styles

--- portfolio-website/src/app/components/HeroSection.jsx
+++ portfolio-website/src/app/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <section className="text-center sm:text-left min-h-screen p-4 lg:p-10 relative">
+    <section className="text-center sm:text-left min-h-screen p-2 sm:p-4 lg:p-10 relative">
       {/* Background layer */}
       <div
         className="absolute inset-0 bg-[url('/hero-image.png')] bg-cover bg-center opacity-40 z-0"
@@ -17,7 +17,7 @@ const HeroSection = () => {
       {/* Content layer */}
       <div className="relative z-20 mt-72 sm:mt-80">
         <div className="sm:px-4">
-          <h1 className="text-white mb-4 text-2xl sm:text-4xl md:text-6xl font-extrabold">
+          <h1 className="text-white mb-4 text-[1.8rem] sm:text-4xl md:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               Hello, I'm{" "}
             </span>
@@ -30,7 +30,7 @@ const HeroSection = () => {
             />
           </h1>
 
-          <p className="text-[#ADB7BE] text-sm lg:text-xl w-full lg:w-1/2">
+          <p className="text-[#ADB7BE] text-sm md:text-lg lg:text-xl w-full lg:w-1/2">
             I specialize in crafting scalable web applications with clean,
             efficient code. I thrive on solving real-world problems through
             thoughtful design and modern technologies.
