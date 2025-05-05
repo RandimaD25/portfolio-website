3f0c7056d22eea8efc69ee92524c047bbe6f8d70
RandimaD25 <randimamethminid525@gmail.com>
2025-05-05 18:16:23 +0530

    feat: implement about me section

--- portfolio-website/src/app/components/HeroSection.jsx
+++ portfolio-website/src/app/components/HeroSection.jsx
@@ -15,9 +15,9 @@ const HeroSection = () => {
       <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/10 to-transparent z-10"></div>
 
       {/* Content layer */}
-      <div className="relative z-20 mt-72 sm:mt-80">
+      <div className="relative z-20 mt-72 sm:mt-80 px-6 sm:px-16">
         <div className="sm:px-4">
-          <h1 className="text-white mb-4 text-[1.8rem] sm:text-4xl md:text-6xl font-extrabold">
+          <h1 className="text-white mb-4 text-[1.7rem] sm:text-4xl md:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               Hello, I'm{" "}
             </span>
