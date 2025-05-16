6ffebb6ccdcbd7f704a7eeea024e95a87cf14e44
RandimaD25 <randimamethminid525@gmail.com>
2025-05-16 12:24:57 +0530

    feat: add animation to the project section

--- portfolio-website/src/app/components/ProjectSection.jsx
+++ portfolio-website/src/app/components/ProjectSection.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+"use client";
+
+import React, { useRef } from "react";
 import { projectData } from "../content/project-data";
 import ProjectCard from "./ProjectCard";
+import { animate, motion, useInView } from "framer-motion";
 
 const ProjectSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
   return (
-    <>
+    <section>
       <div className="text-white px-6 sm:px-16">
         <h2 className="text-4xl md:text-5xl font-bold text-transparent px-4 xl:px-16 bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-pink-600 mb-4 mt-10">
           My Projects
         </h2>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 xl:px-16">
-          {projectData
-            .map((project) => (
+        <ul
+          ref={ref}
+          className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 xl:px-16"
+        >
+          {[...projectData].reverse().map((project, index) => (
+            <motion.li
+              key={index}
+              variants={cardVariants}
+              initial="initial"
+              animate={isInView ? "animate" : "initial"}
+              transition={{ duration: 0.3, delay: index * 0.4 }}
+            >
               <ProjectCard
                 key={project.id}
                 title={project.title}
@@ -20,11 +39,11 @@ const ProjectSection = () => {
                 gitUrl={project.gitUrl}
                 previewUrl={project.previewUrl}
               />
-            ))
-            .reverse()}
-        </div>
+            </motion.li>
+          ))}
+        </ul>
       </div>
-    </>
+    </section>
   );
 };
 
