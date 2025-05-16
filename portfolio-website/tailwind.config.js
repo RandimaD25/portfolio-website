2d14fdae22960a172dc3821598e5dc10eb6230ae
RandimaD25 <randimamethminid525@gmail.com>
2025-05-16 11:53:14 +0530

    feat: change styles

--- portfolio-website/tailwind.config.js
+++ portfolio-website/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
 
 module.exports = {
   content: [
@@ -13,6 +14,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      ...colors,
+      primary: colors.purple,
+      secondary: colors.pink,
     },
   },
   plugins: [],
