79b48b0d8ce8ce25fce2237ec13ef20098714861
RandimaD25 <randimamethminid525@gmail.com>
2025-05-18 12:31:58 +0530

    feat: change the animations

--- portfolio-website/src/app/components/HeroSection.jsx
+++ portfolio-website/src/app/components/HeroSection.jsx
@@ -17,8 +17,8 @@ const HeroSection = () => {
       {/* Content layer */}
       <div className="relative z-20 mt-72 sm:mt-80 px-6 sm:px-16">
         <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
           className="sm:px-4"
         >
