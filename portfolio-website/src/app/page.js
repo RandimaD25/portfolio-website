9d7a4fb3b22255a99d4a4de350bdd795bda0f4a2
RandimaD25 <randimamethminid525@gmail.com>
2025-05-20 19:52:27 +0530

    feat: change the code

--- portfolio-website/src/app/page.js
+++ portfolio-website/src/app/page.js
@@ -1,25 +1,3 @@
-// import Image from "next/image";
-// import HeroSection from "./components/HeroSection";
-// import Navbar from "./components/Navbar";
-// import AboutSection from "./components/AboutSection";
-// import ProjectSection from "./components/ProjectSection";
-// import EmailSection from "./components/EmailSection";
-// import Footer from "./components/Footer";
-
-// export default function Home() {
-//   return (
-//     <main className="flex min-h-screen flex-col bg-[#121212]">
-//       <Navbar />
-//       <div className="flex-1 flex flex-col justify-between">
-//         <HeroSection />
-//         <AboutSection />
-//         <ProjectSection />
-//         <EmailSection />
-//       </div>
-//       <Footer />
-//     </main>
-//   );
-// }
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
